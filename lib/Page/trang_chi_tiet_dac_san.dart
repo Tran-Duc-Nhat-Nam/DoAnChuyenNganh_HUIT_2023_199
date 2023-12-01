85391ca5122d87c9ebaa00c76e16b0dd6585e5c7
Coder-17790 <115089598+Coder-17790@users.noreply.github.com>
2023-12-02 00:35:42 +0700

    appdate trang chi tiet

--- lib/Page/trang_chi_tiet_dac_san.dart
+++ lib/Page/trang_chi_tiet_dac_san.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
 import 'package:app_dac_san/Model/hinh_anh.dart';
+import 'package:app_dac_san/Service/thu_vien_widget.dart';
 import 'package:app_dac_san/main.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import '../Service/thu_vien_api.dart';
 
 import '../Model/vung_mien.dart';
+import '../Widget/xemHinh.dart';
+
+
 
 class TrangChiTietDacSan extends StatefulWidget {
   final int maDS;
@@ -20,15 +25,15 @@ class _TrangChiTietDacSanState extends State<TrangChiTietDacSan> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
+              borderRadius: BorderRadius.circular(10.0),
               child: Image.network(
                 getURLImage(dsDacSan[widget.maDS - 1].avatar),
-                width: 500.0, // Điều chỉnh kích thước ảnh theo ý của bạn
+                width: double.infinity, // Điều chỉnh kích thước ảnh theo ý của bạn
                 height: 300,
                 fit: BoxFit
                     .cover, // Có thể điều chỉnh để tùy chỉnh cách hình ảnh phù hợp trong Container
@@ -38,17 +43,54 @@ class _TrangChiTietDacSanState extends State<TrangChiTietDacSan> {
             Text(dsDacSan[widget.maDS - 1].tenDacSan ?? '',
                 style: const TextStyle(
                     fontWeight: FontWeight.w900,
-                    color: Color.fromARGB(255, 67, 168, 140),
+                    color: Color.fromARGB(255, 104, 143, 187),
                     fontSize: 35,
                     fontFamily: 'RobotoBlack')),
-            const SizedBox(height: 5),
+            // const SizedBox(height: 0),
             Text(getMien(dsDacSan[widget.maDS - 1].idMien),
                 style: const TextStyle(
                     fontWeight: FontWeight.w900,
-                    color: Color.fromARGB(225, 104, 143, 187),
+                    color: Color.fromARGB(225, 230, 155, 44),
                     fontSize: 24,
                     fontFamily: 'ExtraBoldItalic')),
-            const SizedBox(height: 28),
+            const SizedBox(height: 20),
+            Container(
+              height: 230,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: getHinhAnh(dsDacSan[widget.maDS - 1].idDacSan ?? 0).length,
+                // itemCount: 10,
+                itemBuilder: (context,index) {
+                  return Container(
+                    width: 320,
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 8, right: 8),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: GestureDetector(
+                        onTap: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                              builder: (context) => xemHinh(getHinhAnh(dsDacSan[widget.maDS - 1].idDacSan ?? 0)[index])),
+                          );
+                        },
+                        child: Hero(
+                          tag: 'hinhDS',
+                          child: Image.network(
+                            getURLImage(getHinhAnh(dsDacSan[widget.maDS - 1].idDacSan ?? 0)[index]),
+                            fit: BoxFit.cover,
+                            width: double.infinity, // Đặt chiều rộng mong muốn
+                            height: double.infinity
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              ),
+            ),
+            const SizedBox(height: 20),
             const Row(
               children: [
                 Expanded(
@@ -57,45 +99,47 @@ class _TrangChiTietDacSanState extends State<TrangChiTietDacSan> {
                       style: TextStyle(
                           fontWeight: FontWeight.w600,
                           decoration: TextDecoration.underline,
-                          color: Color.fromARGB(225, 100, 167, 130),
+                          color: Color.fromARGB(225, 104, 163, 187),
                           fontSize: 28,
                           fontFamily: "RobotoBlack")),
                 ),
               ],
             ),
             Card(
-                color: const Color.fromARGB(255, 159, 205, 200),
+                color: const Color.fromARGB(255, 242, 242, 242),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5)),
                 child: Container(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(dsDacSan[widget.maDS - 1].moTa ?? '',
+                  child: SelectableText(dsDacSan[widget.maDS - 1].moTa ?? '',
+                      textDirection: TextDirection.ltr,
                       textAlign: TextAlign.justify,
                       style: const TextStyle(
                           fontSize: 18,
                           fontFamily: 'RobotoLight',
                           wordSpacing: 1.3,
-                          letterSpacing: 1,
+                          letterSpacing: 0.1,
                           color: Colors.black,
-                          fontWeight: FontWeight.w600)),
+                          fontWeight: FontWeight.w900)),
                 )),
-            const SizedBox(height: 5)
+            const SizedBox(height: 5),
+
           ],
         ),
       ),
     );
   }
 
-  Future<void> getHinhAnh() async {
-    var reponse = await get(
-        Uri.parse('https://cntt199.000webhostapp.com/getHinhAnh.php'));
-    var result = json.decode(utf8.decode(reponse.bodyBytes));
 
-    for (var document in result) {
-      HinhAnh hinhAnh = HinhAnh.fromJson(document);
-      dsHinhAnh.add(hinhAnh);
+
+
+  List<int> getHinhAnh(int idDacSan) {
+    List<int> ds = [];
+    for(var ha in dsHinhAnh) {
+      if (ha.idDacSan == idDacSan)
+        ds.add(ha.idDacSan ?? 1);
     }
-    setState(() {});
+    return ds;
   }
 
   Future<void> getVungMien() async {
---
+++ lib/Widget/xemHinh.dart
@@ -0,0 +1,43 @@
+import 'package:app_dac_san/Service/thu_vien_api.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class xemHinh extends StatelessWidget{
+  final int link;
+  xemHinh(this.link);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          return RotatedBox(
+            quarterTurns: orientation == Orientation.landscape ? 4 : 0,
+            child: InteractiveViewer(
+              maxScale: 5.0, // Giới hạn tốc độ zoom-in
+              minScale: 0.5, // Giới hạn tốc độ zoom-out
+              child: Container(
+                color: Color.fromARGB(225, 104, 163, 187),
+                width: double.infinity,
+                height: 300,
+                alignment: Alignment.center,
+                child: Hero(
+                    tag: 'hinhDS',
+                    child: Image.network(
+                        getURLImage(link),
+                        fit: BoxFit.cover,
+                        width: double.infinity, // Đặt chiều rộng mong muốn
+                        height: 300
+                    )),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+}
